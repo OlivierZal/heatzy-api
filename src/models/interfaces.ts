a4dffb56fe4fa93d2e01b6f2f9f2e6b187c3c1ad
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-11-09 00:12:41 +0100

    improve interface

--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -3,13 +3,13 @@ import type { Attrs } from '../types.js'
 export interface IBaseDeviceModel {
   data: Attrs
   id: string
+  isFirstGen: boolean
+  isFirstPilot: boolean
+  isGlow: boolean
   name: string
 }
 
 export interface IDeviceModel extends IBaseDeviceModel {
-  isFirstGen: boolean
-  isFirstPilot: boolean
-  isGlow: boolean
   productKey: string
   productName: string
   update: (data: Attrs) => void
