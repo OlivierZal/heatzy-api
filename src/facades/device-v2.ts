a597ce6d66c7b344b08d7f902fe07b58cbbaa966
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2025-07-23 17:05:41 +0100

    improve

--- src/facades/device-v2.ts
+++ src/facades/device-v2.ts
@@ -11,26 +11,23 @@ import { DeviceFacade } from './device.ts'
 export class DeviceV2Facade extends DeviceFacade implements IDeviceV2Facade {
   public get derogationEndString(): string | null {
     const { derogationEndDate, derogationMode } = this
-    if (derogationEndDate) {
-      switch (derogationMode) {
-        case DerogationMode.Boost:
-        case DerogationMode.Presence: {
-          return derogationEndDate.toLocaleString(DateTime.TIME_24_SIMPLE)
-        }
-        case DerogationMode.Vacation: {
-          return derogationEndDate.toLocaleString({
-            day: 'numeric',
-            hour: '2-digit',
-            hour12: false,
-            minute: '2-digit',
-            month: 'short',
-          })
-        }
-        case DerogationMode.Off:
-        default:
-      }
-    }
-    return null
+    return derogationEndDate ?
+        {
+          [DerogationMode.Boost]: () =>
+            derogationEndDate.toLocaleString(DateTime.TIME_24_SIMPLE),
+          [DerogationMode.Off]: () => null,
+          [DerogationMode.Presence]: () =>
+            derogationEndDate.toLocaleString(DateTime.TIME_24_SIMPLE),
+          [DerogationMode.Vacation]: () =>
+            derogationEndDate.toLocaleString({
+              day: 'numeric',
+              hour: '2-digit',
+              hour12: false,
+              minute: '2-digit',
+              month: 'short',
+            }),
+        }[derogationMode]()
+      : null
   }
 
   public get derogationMode(): DerogationMode {
